f5c17db65d6415cffcbe29804d388bb09ab2a869
Matthew Singler <matthew.singler@gmail.com>
2021-10-13 13:12:28 -0500

    created POST method for animals

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
+from animals import get_all_animals, get_single_animal, create_animal
 from customers import get_all_customers, get_single_customer
 from employees import get_all_employees, get_single_employee
 from locations import get_all_locations, get_single_location
@@ -125,8 +126,13 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+        post_body = json.loads(post_body)
+        (resource, id) = self.parse_url(self.path)
+
+        new_animal = None
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+        self.wfile.write(f"{new_animal}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
