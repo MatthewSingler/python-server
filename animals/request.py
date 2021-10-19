1138634ff14815865100031e3646e44ff14ff012
Matthew Singler <matthew.singler@gmail.com>
2021-10-19 15:12:29 -0500

    added parseing of animals by location

--- animals/request.py
+++ animals/request.py
@@ -157,3 +157,32 @@ def get_single_animal(id):
                         data['customer_id'])
 
         return json.dumps(animal.__dict__)
+
+
+def get_animal_by_location(location_id):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+        a.id,
+        a.name,
+        a.status,
+        a.breed,
+        a.location_id,
+        a.customer_id
+        FROM Animal a
+        WHERE a.location_id = ?
+        """, (location_id, ))
+
+        animals = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['status'], row['breed'], row['location_id'], row['customer_id'])
+            animals.append(animal.__dict__)
+
+    return json.dumps(animals)
