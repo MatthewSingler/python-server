a7d452e70541c2c5d0f423fd6475a5d0d3de88ca
Matthew Singler <matthew.singler@gmail.com>
2021-10-19 14:32:02 -0500

    prsed url for customer email

--- customers/__init__.py
+++ customers/__init__.py
@@ -1,1 +1,1 @@
-from .request import delete_customer, get_all_customers, get_single_customer, create_customer, update_customer
+from .request import delete_customer, get_all_customers, get_single_customer, create_customer, update_customer, get_customers_by_email
